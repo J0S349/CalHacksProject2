c722ec80696463d58e89e124bb6b7df354f7002f
J0S349 <josebsan96@gmail.com>
2015-10-11 10:00:37 -0700

    Updates

--- app/src/main/java/com/example/jsanchez_garcia/calhacksproject2/MapsActivity.java
+++ app/src/main/java/com/example/jsanchez_garcia/calhacksproject2/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.example.jsanchez_garcia.calhacksproject2;
 
-import android.support.v4.app.FragmentActivity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +14,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private  GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +30,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
 
 
     @Override
@@ -58,6 +54,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public void setUpMap()
     {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
+    }
+
+    public void onSearch(View view) throws IOException {
+        EditText location_tf = (EditText)findViewById(R.id.TFAddress);
+        List<Address> addressList = null;
+        String location = location_tf.getText().toString();
+
+        if(location != null || location != "")
+        {
+            Geocoder geocoder = new Geocoder(this);
+            try{
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            }catch (IOException s)
+            {
+                s.printStackTrace();
+            }
+                Address address = addressList.get(0);
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        }
+    }
+
+    public void changeType(View view)
+    {
+        if(mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL)
+        {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+        else
+        {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
     }
 }
