4803420de9121d8e394d81c6f966630de6a178e7
J0S349 <josebsan96@gmail.com>
2015-10-13 11:33:23 -0700

    Newest updates to the app

--- app/src/main/java/com/example/jsanchez_garcia/calhacksproject2/MapsActivity.java
+++ app/src/main/java/com/example/jsanchez_garcia/calhacksproject2/MapsActivity.java
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -37,10 +38,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(-34, 151);   //this are the cordinates of sydney Australia
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Sydney, Australia").icon(BitmapDescriptorFactory.defaultMarker(213)));    //this will set a marker on the map on Sydney Australia
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney)); //this will shift the center of the camera to the position
     }
+
     public void setUpMapIfNeeded(){
         if(mMap == null)
         {
